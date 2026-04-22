631242ce0202d6764bcdc5082cb279057fe090b6
sentibeitaokong <1120996904@qq.com>
2026-04-22 09:53:44 +0800

    添加prettierrc

--- packages/runtime-core/src/h.ts
+++ packages/runtime-core/src/h.ts
@@ -1,4 +1,4 @@
-import {createVNode} from "./vnode.ts";
-export function h(type:any, props?:any, children?:any):any {
-    return createVNode(type, props, children);
+import { createVNode } from './vnode.ts'
+export function h(type: any, props?: any, children?: any): any {
+  return createVNode(type, props, children)
 }
